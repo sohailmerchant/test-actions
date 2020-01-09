23bb91589d2f2a73d830c098cbdf959b85712a2b
S Merchant <sohailsmerchant@gmail.com>
2020-01-09 16:01:12 +0000

    print

--- test_actions.py
+++ test_actions.py
@@ -2,6 +2,7 @@ import requests
 
 r = requests.get('https://raw.githubusercontent.com/sohailmerchant/kitab-metadata-automation/master/README.md')
 print(r.content)
+print('printing...')
 
 with open('copiedfile..md', 'w') as f:
     f.write(str(r.content))
